994ea64f07e294c54b4745fb03f6814753aead77
Francis Cianfrocca <garbagecat10@gmail.com>
2025-09-29 15:58:38 +0000

    added napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,10 @@ author = 'Francis Cianfrocca'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
