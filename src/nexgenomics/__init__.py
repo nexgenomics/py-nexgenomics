addf437d29860cf8c86f6c865f42d9c08192b1bf
Francis Cianfrocca <garbagecat10@gmail.com>
2025-11-16 23:00:24 +0000

    partial refactoring of agent calls, more coming

--- src/nexgenomics/__init__.py
+++ src/nexgenomics/__init__.py
@@ -3,9 +3,16 @@
 from .webhook import ping
 from .agentstore import Agentstore
 
+from .agents import post_sentence
+from .agents import post_sentences
+from .agents import get_agent_list
+
 __all__ = [
     "ping",
-    "Agentstore"
+    "Agentstore",
+    "get_agent_list"
+    "post_sentence",
+    "post_sentences",
 ]
 
 
--- src/nexgenomics/agents.py
+++ src/nexgenomics/agents.py
@@ -7,13 +7,18 @@ from typing import List,Union
 
 
 def get_agents():
-    url = f"{_internals._get_api_url_stem()}/api/v0/agents"
-    headers = {"Authorization": f"Bearer {_internals._get_api_auth_token()}"}
-    resp = requests.get(url,headers=headers)
-    _internals._handle_api_error(resp)
-    return resp.json()
+    url = "api/v0/agents"
+    return _internals.get (url)
 
 def list():
+    """
+    synonym for get_agents
+    """
+    return get_agents()
+def get_agent_list():
+    """
+    synonym for get_agents
+    """
     return get_agents()
 
 def hire_agent(id:str, *, title:str="", desc:str=""):
@@ -134,4 +139,40 @@ def post_agent_sentence(id:str,sentence:str):
 class Agent:
     def __init__(self):
         print ("hello")
+
+
+
+
+
+def post_sentence(sentence:str):
+    """
+    Send a single sentence up to an agent which is identified by the agent token
+    specified in the runtime (which can be config file, environment, or programmatic).
+    This supports high-security environments where only the agent token is available,
+    by intentionally avoiding the need for an agent id or a principal-auth token.
+    """
+    #url = f"{_internals._get_api_url_stem()}/api/v0/agent/{id}/sentence"
+    url = "api/v0/agent/sentence"
+    s2 = sentence.replace("\r\n","\n")
+    return _internals.post (url=url, content_type="application/octet-stream", data=s2.encode('utf-8'))
+
+def post_sentences(sentences:Union[List[str],List[bytes]]):
+    """
+    Send a list of sentences up to an agent which is identified by the agent token
+    specified in the runtime.
+    """
+    r = []
+    for s in sentences:
+        if isinstance(s,str):
+            r.append(s)
+        elif isinstance(s,bytes):
+            r.append(s.decode('utf-8',errors='replace'))
+        else:
+            raise TypeError (f"item must be str or bytes, got {type(s)}")
+
+    r2 = [s.replace("\r\n","\n") for s in r]
+    r3 = "\n".join(r2)+"\n"
+
+    url = "api/v0/agent/sentences"
+    return _internals.post(url=url,data=r3.encode('utf-8'),content_type="application/octet-stream")
 
